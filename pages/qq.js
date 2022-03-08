d05c2220bc46fbf034dae99d4396d72b2088a7b4
黎旭 <1300311464@qq.com>
2022-03-08 17:24:19 +0800

    Update qq.js

--- pages/qq.js
+++ pages/qq.js
@@ -1,5 +1,4 @@
-<script src="https://unpkg.com/react@15/dist/react.js"></script>
-<script src="https://unpkg.com/react-dom@15/dist/react-dom.js"></script>
+
 
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
